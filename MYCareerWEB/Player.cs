e578b53106de3b5de48fffe9cf8415930ac5b53b
Edair <adamgiecu@gmail.com>
2022-07-02 01:14:11 +0200

    Added player creation.

---
+++ MYCareerWEB/Player.cs
@@ -0,0 +1,20 @@
+﻿namespace MYCareerWEB
+{
+    public class Player
+    {
+        public string FirstName { get; private set; }
+        public string NickName { get; private set; }
+        public string LastName { get; private set; }
+        public int Position { get; private set; }
+        public int Height { get; private set; }
+
+        public Player(string firstName, string nickName, string lastName, int position, int height)
+        {
+            FirstName = firstName;
+            NickName = nickName;
+            LastName = lastName;
+            Position = position;
+            Height = height;
+        }
+    }
+}
